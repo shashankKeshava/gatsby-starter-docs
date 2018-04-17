e1843f4966b68dab206805465ec1c697b85805b5
Shashank <keshavashashank@gmail.com>
2018-04-17 22:42:06 +0530

    Changed user configs

--- src/components/UserLinks.jsx
+++ src/components/UserLinks.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import { FaGithubAlt, FaTwitter } from "react-icons/lib/fa";
-import { MdMessage, MdEmail } from "react-icons/lib/md";
+import React, {Component} from "react";
+import {FaGithubAlt, FaTwitter} from "react-icons/lib/fa";
+import {MdMessage, MdEmail} from "react-icons/lib/md";
 import styled from "styled-components";
 
-const Container = styled.div`
+const Container = styled.div `
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -11,7 +11,7 @@ const Container = styled.div`
   max-width: 100%;
 `;
 
-const UserIcon = styled.a`
+const UserIcon = styled.a `
   margin-left: 25px;
   color: white;
   &:hover {
@@ -29,17 +29,17 @@ class UserLinks extends Component {
   render() {
     return (
       <Container className="user-links">
-        <UserIcon href="https://github.com/ericwindmill">
-          <FaGithubAlt style={iconStyle} />
+        <UserIcon href="https://github.com/shashankKeshava/">
+          <FaGithubAlt style={iconStyle}/>
         </UserIcon>
-        <UserIcon href="https://twitter.com/ericwindmill?lang=en">
-          <FaTwitter style={iconStyle} />
+        <UserIcon href="https://twitter.com/keshavashashank">
+          <FaTwitter style={iconStyle}/>
         </UserIcon>
-        <UserIcon href="https://www.messenger.com/t/eric.windmill">
-          <MdMessage style={iconStyle} />
-        </UserIcon>
-        <UserIcon href="mailto:eric@ericwindmill.com">
-          <MdEmail style={iconStyle} />
+        {/* <UserIcon href="https://www.messenger.com/t/eric.windmill">
+          <MdMessage style={iconStyle}/>
+        </UserIcon> */}
+        <UserIcon href="mailto:keshavashashank@gmail.com">
+          <MdEmail style={iconStyle}/>
         </UserIcon>
       </Container>
     );
